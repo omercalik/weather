ceb8716f5435f289dd0768305d8872afbd48f010
vignerd <omerbcalik@gmail.com>
2021-08-10 00:16:56 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -35,42 +35,46 @@ export default defineComponent({
 
   methods: {
     async fetchWeatherData() {
-      // let location = await axios.get(
-      //   `https://freegeoip.app/json/`,
-      //   auth
-      // )
+      let location = await axios.get(
+        `https://get.geojs.io/v1/ip/geo.json`,
+        
+      )
 
-      // console.log(location)
+      
 
 
-      // let id = await axios.get(
-      //   `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/search/?query=${location.data.city}`,
-      //   auth
-      // )
+      let id = await axios.get(
+        `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/search/?query=${location.data.city}`,
+        auth
+      )
 
-      // if (id.data.length > 0) {
-      //   let weather = await axios.get(
-      //     `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/${id.data[0].woeid}`,
-      //     auth
-      //   )
-
-      //   console.log(weather)
-
-      //   this.weatherData = weather.data
-      // } else {
+      if (id.data.length > 0) {
         let weather = await axios.get(
-          `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/44418`,
+          `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/${id.data[0].woeid}`,
+          auth
+        )
+
+        console.log(weather)
+
+        this.weatherData = weather.data
+      } else {
+        let weather = await axios.get(
+          `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_API_BASE_URL}/location/2343732`,
           auth
         )
 
         this.weatherData = weather.data
-     // }
+     }
     },
 
     changeLocation(newData: any) {
       console.log(newData)
       this.weatherData = newData
     },
+
+    getLocation(){
+
+    }
   },
 })
 </script>
